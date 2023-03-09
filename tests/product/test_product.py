0f1aa1cd1cfb7c993929b37524732b661e651c2f
adevmir <ademirvalonqar@gmail.com>
2023-03-09 11:21:58 -0300

    [Req - 01] - Implementa teste em test_product.py que verifica criacao de instancia do objeto Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,31 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
     pass  # Seu teste deve ser escrito aqui
+
+    id = 1
+    nome_do_produto = "eclado Mecanico"
+    nome_da_empresa = "Devmir_LTDA"
+    data_de_fabricacao = "11/04/2022"
+    data_de_validade = "11/04/2026"
+    numero_de_serie = "758493"
+    inst_de_armazenamento = "em local sem humidade"
+
+    product_obj = Product(
+        id,
+        nome_do_produto,
+        nome_da_empresa,
+        data_de_fabricacao,
+        data_de_validade,
+        numero_de_serie,
+        inst_de_armazenamento,
+    )
+
+    assert product_obj.id == id
+    assert product_obj.nome_do_produto == nome_do_produto
+    assert product_obj.nome_da_empresa == nome_da_empresa
+    assert product_obj.data_de_fabricacao == data_de_fabricacao
+    assert product_obj.data_de_validade == data_de_validade
+    assert product_obj.numero_de_serie == numero_de_serie
+    assert product_obj.instrucoes_de_armazenamento == inst_de_armazenamento
