74bee17a21a0b46e7b7632e23d4dff38ac2849cb
adevmir <ademirvalonqar@gmail.com>
2023-03-10 11:36:38 -0300

    [Req 02] - Recebe uma lista de dicionarios de produtos e retorna a data de fabricacao mais antiga, a data de validade mais proxima e o nome da empresa com maior ocorrencia de produtos

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -3,18 +3,20 @@ from datetime import date
 
 class SimpleReport:
 
-    def generate(list: list[dict]):
+    def generate(list):
         fabrication_date = [product["data_de_fabricacao"] for product in list]
         expiration_date = [product["data_de_validade"] for product in list
-                           if product["data_de_validade"] > date.today()]
+                           if product["data_de_validade"] >
+                           date.today().strftime("%Y-%m-%d")]
         company_list = [product["nome_da_empresa"] for product in list]
         company_name = ""
         n = 0
         for company in company_list:
-            freq = company_list.counter(company)
+            freq = company_list.count(company)
             if freq > n:
                 company_name = company
                 n = freq
-        return (f"Data de fabricação mais antiga: {min(fabrication_date)}"
-                f"Data de validade mais próxima: {min(expiration_date)}"
+
+        return (f"Data de fabricação mais antiga: {min(fabrication_date)}\n"
+                f"Data de validade mais próxima: {min(expiration_date)}\n"
                 f"Empresa com mais produtos: {company_name}")
