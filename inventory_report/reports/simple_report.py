665c1b90129e83014c00b7eb760e145a074dce3e
adevmir <ademirvalonqar@gmail.com>
2023-03-10 11:24:32 -0300

    [Req 02] - 01

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,12 +1,12 @@
 from datetime import date
 
+
 class SimpleReport:
-    pass
-    @staticmethod
+
     def generate(list: list[dict]):
         fabrication_date = [product["data_de_fabricacao"] for product in list]
         expiration_date = [product["data_de_validade"] for product in list
-                    if product["data_de_validade"] > date.today()]
+                           if product["data_de_validade"] > date.today()]
         company_list = [product["nome_da_empresa"] for product in list]
         company_name = ""
         n = 0
@@ -15,6 +15,6 @@ class SimpleReport:
             if freq > n:
                 company_name = company
                 n = freq
-        return f""" Data de fabricação mais antiga: {min(earliest_fab_date)}
-                    Data de validade mais próxima: {min(closest_exp_date)}
-                    Empresa com mais produtos: {company_name} """
+        return (f"Data de fabricação mais antiga: {min(fabrication_date)}"
+                f"Data de validade mais próxima: {min(expiration_date)}"
+                f"Empresa com mais produtos: {company_name}")
