8513249726c1163646afb52acf79aee3a091501b
adevmir <ademirvalonqar@gmail.com>
2023-03-11 15:19:13 -0300

    [Req 03] - Implementa funcao CompleteReport que herda SimpleReport e gera um relatorio completo com o parametro generate

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,23 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+
+    def generate(list):
+        simple_report = SimpleReport.generate(list)
+        list_simple_report = simple_report.split(":")
+        length = len(list_simple_report)
+        company = str(list_simple_report[length-1]).strip()
+        products_in_stock = [product["nome_do_produto"] for product in list
+                             if product["nome_da_empresa"] == company]
+        products_dict = {}
+        for product in products_in_stock:
+            quant = products_in_stock.count(product)
+            products_dict[product] = quant
+        products_string = ""
+        for product, quant in products_dict.items():
+            products_string += f"- {product}: {quant}\n"
+        return (f"{simple_report}\n"
+                f"Produtos estocados por empresa:\n"
+                f"- {company}: {len(products_in_stock)}\n"
+                f"{products_string}")
