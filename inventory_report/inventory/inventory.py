ae7b1f4c72eb7f397bb3881b05ed1e2af1ee61d2
adevmir <ademirvalonqar@gmail.com>
2023-03-11 17:34:51 -0300

    [Req 05] - Atualiza Inventory() para que funcione com arquivos .json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,17 +1,26 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory():
-
-    def import_data(path: str, report_type: str) -> str:
-        product_list = []
-        with open(path) as file:
-            products_reader = csv.DictReader(file)
+    @staticmethod
+    def read(path):
+        list_products = []
+        with open(path, encoding="utf-8") as file:
+            if "csv" in path:
+                products_reader = csv.DictReader(file)
+            if "json" in path:
+                products_reader = json.load(file)
             for product in products_reader:
-                product_list.append(product)
+                list_products.append(product)
+        return list_products
+
+    @classmethod
+    def import_data(cls, path, report_type):
+        list_products = cls.read(path)
         if report_type == "simples":
-            return SimpleReport.generate(product_list)
-        if report_type == "completo":
-            return CompleteReport.generate(product_list)
+            return SimpleReport.generate(list_products)
+        else:
+            return CompleteReport.generate(list_products)
