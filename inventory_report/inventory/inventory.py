cb132c66e32d019c3ce160bb019d5ee50c98777c
adevmir <ademirvalonqar@gmail.com>
2023-03-11 17:47:39 -0300

    [req - 06] - refatora Inventory para que funcione com arquivo .xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,20 +2,23 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xmltodict
 
 
 class Inventory():
     @staticmethod
     def read(path):
-        list_products = []
         with open(path, encoding="utf-8") as file:
             if "csv" in path:
                 products_reader = csv.DictReader(file)
+                list_products = []
+                for product in products_reader:
+                    list_products.append(product)
+                return list_products
             if "json" in path:
-                products_reader = json.load(file)
-            for product in products_reader:
-                list_products.append(product)
-        return list_products
+                return json.load(file)
+            else:
+                return xmltodict.parse(file.read())["dataset"]["record"]
 
     @classmethod
     def import_data(cls, path, report_type):
