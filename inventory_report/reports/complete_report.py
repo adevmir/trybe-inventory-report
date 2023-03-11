3094d938aba0e1bc3abeef3371e698c71385ddc8
adevmir <ademirvalonqar@gmail.com>
2023-03-11 16:28:16 -0300

    [Req 03] - Refatorando e corrigindo erro da funcao

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -3,21 +3,18 @@ from inventory_report.reports.simple_report import SimpleReport
 
 class CompleteReport(SimpleReport):
 
-    def generate(list):
+    def generate(list) -> str:
         simple_report = SimpleReport.generate(list)
-        list_simple_report = simple_report.split(":")
-        length = len(list_simple_report)
-        company = str(list_simple_report[length-1]).strip()
-        products_in_stock = [product["nome_do_produto"] for product in list
-                             if product["nome_da_empresa"] == company]
         products_dict = {}
-        for product in products_in_stock:
-            quant = products_in_stock.count(product)
-            products_dict[product] = quant
+        for product in list:
+            company = product['nome_da_empresa']
+            products_dict[company] = products_dict.get(company, 0) + 1
         products_string = ""
-        for product, quant in products_dict.items():
-            products_string += f"- {product}: {quant}\n"
+        for company, quant in products_dict.items():
+            products_string += f"- {company}: {quant}\n"
+        print(f"{simple_report}\n"
+                f"Produtos estocados por empresa:\n"
+                f"{products_string}")
         return (f"{simple_report}\n"
                 f"Produtos estocados por empresa:\n"
-                f"- {company}: {len(products_in_stock)}\n"
                 f"{products_string}")
